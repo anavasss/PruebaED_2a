e01578aebe05f6a1bfce36467a4c723e85eba9b4
anavasss <alfonsitonavajas1@gmail.com>
2025-03-16 16:42:08 +0100

    documentado Curso

--- src/instituto/Curso.java
+++ src/instituto/Curso.java
@@ -12,24 +12,33 @@ import java.util.TreeSet;
  */
 public class Curso {
 
+	/*
+	 * Variables (un string que es el nombre, y una coleccion de todos los alumnos).
+	 */
     private String nombre;
     private TreeSet<Persona> listaAlumnos;
 
     /*
-    Documentar y crear test unitario
-    */
+     * Documentar y crear test unitario
+     * (Se devuelve el nombre del curso).
+     */
     protected String getNombre() {
         return nombre;
     }
 
+    
     /*
-    Documentar y crear test unitario
-    */
+     * Documentar y crear test unitario
+     * (Se crea el curso con el nombre del mismo).
+     */
     public Curso(String nombre) {
         this.nombre = nombre;
         listaAlumnos = new TreeSet<>();
     }
 
+    /*
+     * Se hace un toString para mostrar por pantalla posteriormente.
+     */
     @Override
     public String toString() {
         String s = "--------------------" + nombre + "-----------------\n";
@@ -41,6 +50,9 @@ public class Curso {
         return s;
     }
 
+    /*
+     * Se manda la persona que se quiere añadir al curso y se añade.
+     */
     public void aniadirAlumno(Persona p) {
         listaAlumnos.add(p);
     }
